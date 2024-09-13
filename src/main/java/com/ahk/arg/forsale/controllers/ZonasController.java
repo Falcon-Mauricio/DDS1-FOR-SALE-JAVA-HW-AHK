5847aae4cf2a6472e79eb790b1a102d6ffa63959
Falcon_p <mauriciofalcon1304@gmail.com>
2024-09-13 11:07:10 -0300

    1

--- src/main/java/com/ahk/arg/forsale/controllers/ZonasController.java
+++ src/main/java/com/ahk/arg/forsale/controllers/ZonasController.java
@@ -1,37 +1,16 @@
 package com.ahk.arg.forsale.controllers;
 
-import com.ahk.arg.forsale.models.entities.Zona;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
+@Controller
 @RequestMapping("/zonas")
 public class ZonasController {
 
-    private List<Zona> zonas;
-
-    public ZonasController(){
-        this.zonas = new ArrayList<>();
-        Zona zona1 = new Zona();
-        zona1.setNombre("Recoleta");
-
-        this.zonas.add(zona1);
-    }
     @GetMapping
-    public ResponseEntity<List<Zona>> buscarTodos() {
-        return new ResponseEntity<>(this.zonas, HttpStatus.OK);
+    public ModelAndView listadoDeZonas(){
+        return new ModelAndView("zonas");
     }
-
-    public ResponseEntity<Zona> crearZona(@RequestBody Zona zona){
-        this.zonas.add(zona);
-        return new ResponseEntity<>(zona, HttpStatus.CREATED);
-    }
-
 }
--- src/main/java/com/ahk/arg/forsale/models/entities/Zona.java
+++ src/main/java/com/ahk/arg/forsale/models/entities/Zona.java
@@ -8,20 +8,5 @@ import lombok.Setter;
 public class Zona {
     private String nombre;
     private Float precio;
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setPrecio(Float precio) {
-        this.precio = precio;
-    }
-
-    public Float getPrecio() {
-        return precio;
-    }
+    private Integer id;
 }
